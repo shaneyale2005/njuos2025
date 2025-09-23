a3e1eda5f8f5476704fb80cb566bb5aaa772f197
shaneyale <zeta186012@gmail.com>
2025-09-23 21:36:50 +0800

    implemented two functions in lab1

--- lab1/labyrinth/labyrinth.c
+++ lab1/labyrinth/labyrinth.c
@@ -21,17 +21,52 @@ void printUsage() {
 
 bool isValidPlayer(char playerId) {
     // TODO: Implement this function
-    return false;
+    // 这个函数用来判断是否是一个有效的玩家
+    return (playerId >= '0'&& playerId <= '9');
 }
 
 bool loadMap(Labyrinth *labyrinth, const char *filename) {
     // TODO: Implement this function
-    return false;
+    // 注意这里的打开文件的方式，关于文件需要复习
+    FILE *fp = fopen(filename, 'r');
+    if (!fp) {
+        return false;
+    }
+    labyrinth -> rows = 0;
+    // 在这里设置一个缓冲区，这很重要，要稍微大一点点
+    char buffer [MAX_COLS + 2];
+
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        // 下面这段逻辑是用来去掉末尾的换行符的
+        int len = strlen(buffer);
+        if (buffer[len - 1] == '\n') {
+            buffer[len - 1] = '\0';
+            len -= 1;
+        }
+
+        // 接下来的操作是拷贝到map中
+        strcpy(labyrinth->map[labyrinth->rows], buffer);
+        labyrinth -> rows ++;
+        labyrinth -> cols = len; // 这里是假设所有行的长度都一样的
+    }
+
+    fclose(fp);
+    return (labyrinth -> rows > 0);
 }
 
 Position findPlayer(Labyrinth *labyrinth, char playerId) {
     // TODO: Implement this function
     Position pos = {-1, -1};
+    // 可以直接进行判断，前提是需要熟悉Labyrinth这个数据结构
+    for (int i = 0; i < labyrinth -> rows; i++) {
+        for (int j = 0; j < labyrinth -> cols; j++) {
+            if (playerId == labyrinth -> map[i][j]) {
+                pos.row = i;
+                pos.col = j;
+                return pos;
+            }
+        }
+    }
     return pos;
 }
 
